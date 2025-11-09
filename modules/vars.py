136f72f8bce4e9e05081dd24bfee9f41cf35e9fd
azzupsmeena1166-ops <azzupsmeena1166@gmail.com>
2025-11-09 23:43:26 +0530

    Set default values for environment variables
    Updated environment variable defaults for api_id, api_hash, bot_token, and CHANNEL_ID.

--- modules/vars.py
+++ modules/vars.py
@@ -1,9 +1,10 @@
 from os import getenv
 
-api_id    = int(getenv("api_id", ""))
-api_hash  = getenv("api_hash", "")
-bot_token = getenv("bot_token", "")
+api_id    = int(getenv("api_id", "33490515"))
+api_hash  = getenv("api_hash", "378e1c46ebe4f29292a91260866df8a4")
+bot_token = getenv("bot_token", "8274532621:AAF7o_4S9WSqqok9qbukNUQLWWO3_gbhatU")
 pwtoken = getenv("pwtoken", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1003241105977"))
 AUTH = ""
 #Bot Username = ""
+
